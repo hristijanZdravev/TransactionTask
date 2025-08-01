d76bd4357efe4a9f51c09f89865c1f93080ec535
Hristijan Zdravev <hristijan.zdravev@markantsoftware.com>
2025-08-01 14:22:09 +0200

    updated readme

--- DTOs/FeeRuleDTO.cs
+++ DTOs/FeeRuleDTO.cs
@@ -5,7 +5,7 @@ namespace TransactionTask.DTOs
     public class FeeRuleDTO
     {
        public int Id { get; set; }
-       public string Name { get; set; }
+       public string? Name { get; set; }
 
         internal FeeRuleDTO toDTO(FeeRule fr)
         {
